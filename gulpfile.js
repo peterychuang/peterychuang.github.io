65ad910c9eaf4c291464b2d7956ec3f14b32627b
Peter Y. Chuang <peterychuang@users.noreply.github.com>
2016-08-07 12:18:20 -0400

    Build with Travis CI

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task('copy1', ['minify2'], function() {
     './_output/hugo-stage-1/assets/**/*',
     '!./_output/hugo-stage-1/assets/img/t/**/*',
     '!./_output/hugo-stage-1/assets/img/t/'])
-  .pipe(gulp.dest('/var/www/assets'))
+  .pipe(gulp.dest('./public/assets'))
 });
 
 gulp.task('copy2', ['copy1'], function() {
@@ -53,28 +53,21 @@ gulp.task('copy2', ['copy1'], function() {
     './_output/hugo-stage-1/robots.txt',
     './_output/hugo-stage-1/sitemap.xml'
   ])
-  .pipe(gulp.dest('/var/www/'))
+  .pipe(gulp.dest('./public/'))
 });
 
 gulp.task('copy3', ['copy2'], function() {
   return gulp.src([
     './_output/hugo-stage-1/categories/*/index.xml'
   ])
-  .pipe(gulp.dest('/var/www/'))
+  .pipe(gulp.dest('./public/'))
 });
 
 gulp.task('copy4', ['copy3'], function() {
   return gulp.src([
     './_output/hugo-stage-1/tag/*/index.xml'
   ])
-  .pipe(gulp.dest('/var/www/tag/'))
-});
-
-gulp.task('copy4', ['copy3'], function() {
-  return gulp.src([
-    './_output/hugo-stage-1/tag/*/index.xml'
-  ])
-  .pipe(gulp.dest('/var/www/tag/'))
+  .pipe(gulp.dest('./public/tag/'))
 });
 
 gulp.task('copy5', ['copy4'], function() {
@@ -82,7 +75,14 @@ gulp.task('copy5', ['copy4'], function() {
     './_output/hugo-stage-1/*/index.xml',
     '!./master-preprocessed/categories/index.xml'
   ])
-  .pipe(gulp.dest('/var/www/'))
+  .pipe(gulp.dest('./public/'))
+});
+
+gulp.task('copy6', ['copy5'], function() {
+  return gulp.src([
+    './_output/hugo-stage-1/readme.md'
+  ])
+  .pipe(gulp.dest('./public/'))
 });
 
 gulp.task('base64', ['copy5'], function () {
@@ -94,8 +94,7 @@ gulp.task('base64', ['copy5'], function () {
     extensions: ['jpg'],
     maxImageSize: 20 * 1024 // bytes
   }))
-  .pipe(gulp.dest('/var/www/'));
+  .pipe(gulp.dest('./public/'));
 });
 
-gulp.task('deploy', ['minify1', 'minify2', 'copy1', 'copy2', 'copy3', 'copy4', 'copy5', 'base64']);
-
+gulp.task('deploy', ['minify1', 'minify2', 'copy1', 'copy2', 'copy3', 'copy4', 'copy5', 'copy6', 'base64']);
