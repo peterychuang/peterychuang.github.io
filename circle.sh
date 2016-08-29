a9bb3c88a99ce7e218aba3ef768eb1a4fb87effa
Peter Y. Chuang <peteryuchuang@gmail.com>
2016-08-29 17:38:21 -0400

    Change CircleCI Scripts

--- circle.sh
+++ circle.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "# This file is a template, and might need editing before it works on your project.\n# Full project: https://gitlab.com/pages/plain-html\npages:\n  stage: deploy\n  script:\n  - mkdir .public\n  - cp -r * .public\n  - mv .public public\n  artifacts:\n    paths:\n    - public\n  only:\n  - master" > ~/peterychuang.github.io/public/.gitlab-ci.yml
 echo "# [Peter Y. Chuang - Novelist](https://novelist.xyz)\n\nThis is the repository for the compiled HTML and static files of my website.\n\nFor the source of the website, please visit the [source branch](https://github.com/peterychuang/peterychuang.github.io/tree/source)." > ~/peterychuang.github.io/public/readme.md
 REV=$(git rev-parse HEAD)
 mkdir ~/tmp
@@ -10,8 +9,9 @@ ssh-add ~/.ssh/id_gitlab.com
 git config --global user.email "peteryuchuang@gmail.com"
 git config --global user.name "Peter Y. Chuang"
 git clone --branch master git@gitlab.com:peterychuang/peterychuang.gitlab.io.git ~/tmp/public
-rsync -rt --delete --exclude=".git" ~/peterychuang.github.io/public ~/tmp
+rsync -rt --delete --exclude=".git" --exclude=".gitlab-ci.yml" ~/peterychuang.github.io/public ~/tmp
 cd ~/tmp/public
+echo .gitlab-ci.yml > .gitignore
 git add .
 git commit --allow-empty -m "Built from commit $REV"
 git push
